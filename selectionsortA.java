723672477887a23f028ddd9e36e52cd0bac25214
WhiteDevil1910 <143121219+WhiteDevil1910@users.noreply.github.com>
2023-08-26 01:52:38 +0530

    Add files via upload

--- selectionsortA.java
+++ selectionsortA.java
@@ -1,6 +1,7 @@
 package selectionsort;
 import java.util.*;
 public class selectionsortA {
+	
 	public static void selectionSort(int[] nums) 
 	{
 	 for (int i = 0; i < nums.length; i++) 
@@ -20,34 +21,74 @@ public class selectionsortA {
 	 nums[min] = swap;
 	 }
 	}
-	public static void main(String[] args) 
+	
+	public static void main(String args[])
 	{
-	 int[] array = new int[10000];
-	 for (int i = 0; i < array.length; i++) {
-	 array[i] = i;
-	 }
-	 // Shuffle array
-	 Collections.shuffle(Arrays.asList(array));
-	 // Print shuffled collection
-	 System.out.println(" Unsorted Array List is:")
-	 System.out.println(\tArrays.toString(array));
-	 long startTime = System.nanoTime();
-	System.out.println("Selection Sort"); 
-	selectionSort(array);
-	 long endTime = System.nanoTime();
-	 // Print sorted collection
-	System.out.println(" the Sorted Array is:");
-	 System.out.println(\tArrays.toString(array));
-	 // Print runtime in seconds
-	 System.out.println("Selection Sort runtime: " + (endTime - startTime)/1000000000);
+	int max=50000;
+	int n,i;
+	Scanner in=new Scanner(System.in);
+	Random rand=new Random();
+	System.out.println("Selectionsort Test");
+	/* Accept no.of Elements */
+	System.out.println("\nEnter the number of elements");
+	n=in.nextInt();
+	/* create array of n elements */
+	int arr[]=new int[n];
+	try
+	{
+	/* Generate Random Numbers */
+	for(i=0;i<n;i++)
+	arr[i]=rand.nextInt(100);
+	/* Print random numbers */
+	System.out.println("\nthe random elements are ");
+	for(i=0;i<n;i++)
+	System.out.println(arr[i]+" ");
+	long start_time=System.nanoTime();
+	/*call method Selection Sort*/
+	selectionSort(arr);
+	long end_time=System.nanoTime();
+	/* Print Sorted Array */
+	System.out.println("\nThe Elements After sorting");
+	for(i=0;i<n;i++)
+	System.out.println(arr[i]+" ");
+	long t=end_time - start_time;
+	System.out.println("Time taken for execution is:"+t+" nanoseconds");
+	}
+	catch(ArrayIndexOutOfBoundsException ae)
+	{
+	System.out.println("Array Index reached maximum ");
+	}
 	}
 
 }
+/*
+Selectionsort Test
 
-/*Output:
- * Unsorted Array List is: ---------
-the Sorted Array is: 0 1 2 3…..10000
-Selection Sort runtime: ---
+Enter the number of elements
+10
+
+the random elements are 
+74 
+16 
+87 
+71 
+59 
+29 
+56 
+95 
+53 
+15 
+
+The Elements After sorting
+15 
+16 
+29 
+53 
+56 
+59 
+71 
+74 
+87 
+95 
+Time taken for execution is:4000 nanoseconds
 */
- */
- 
